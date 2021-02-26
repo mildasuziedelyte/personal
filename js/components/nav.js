b138aff14c3403b86b65d7531d4da30bed646cb7
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2021-02-26 17:26:18 +0200

    skills & portfolio updt

--- js/components/nav.js
+++ js/components/nav.js
@@ -2,24 +2,33 @@ function nav(){
 
     const nav = document.querySelectorAll('nav > a');
 
+    // document.getElementById("and").addEventListener("mouseover", () => {
+    //     document.getElementById("and").style.opacity="0.7"
+    // })
+
+    // document.getElementById("and").addEventListener("mouseout", () => {
+    //     document.getElementById("and").style.opacity="1"
+    // })
+
     nav.forEach(link => {
-        link.addEventListener('mouseover', () => {
-            link.classList.add('nav-hover');
+        link.addEventListener("mouseover", () => {
+            link.classList.add("nav-hover");
             nav.forEach(link => {
                 link.style.opacity="0.7"
             })
+            document.getElementById("and").style.opacity="0.7"
             link.style.opacity="1";
 
         })  
        link.addEventListener("mouseout", () => {
-            link.classList.remove('nav-hover');
+            link.classList.remove("nav-hover");
             nav.forEach(link => {
                 link.style.opacity="1"
             })
+            document.getElementById("and").style.opacity="1"
             // link.style.opacity="1";
 
         })  
-
     })
 
 }
